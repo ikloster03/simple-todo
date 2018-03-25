e5ce35717b0132dab61dd6a69e0986372d3daf23
Monastyrev Ivan <ikloster@yandex.ru>
2018-03-25 10:16:42 +0300

    Update App.test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('renders without crashing', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+  
+  it('Welcome to React', () => {
+    const p = wrapper.find('h1.App-title')
+    expect(p.text()).toBe('Welcome to React')
+  });
+})
