24f91b2bd6414b43752745c80133a981cd337d11
Monastyrev Ivan <ikloster@yandex.ru>
2018-03-25 11:24:33 +0300

    Update App.test

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+enzyme.configure({ adapter: new Adapter() });
+
 
 describe('renders without crashing', () => {
   it('renders without crashing', () => {
@@ -10,7 +14,8 @@ describe('renders without crashing', () => {
   });
   
   it('Welcome to React', () => {
+    const wrapper = enzyme.mount(<App />);
     const p = wrapper.find('h1.App-title')
-    expect(p.text()).toBe('Welcome to React')
+    expect(p.text()).toBe('Welcome to React!!')
   });
 })
